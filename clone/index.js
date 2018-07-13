c24701f8a7018baf08245e25c1cd7dcd51b9e5cb
youzyy <41198809+youzyy@users.noreply.github.com>
2018-07-13 16:40:27 +0200

    Add files via upload

---
+++ clone/index.js
@@ -0,0 +1,79 @@
+
+        $("#group").click(function(){
+            $("#text").toggle("slow");
+        });
+        $("#g").click(function(){
+            $("#tex").toggle("slow");
+        });
+
+        $("#gg").click(function(){
+            $("#te").toggle("slow");
+        })
+       
+        $(document).ready(function(){
+            $("#y").click(function(){
+                $("#block1").animate({
+                    left: '250px',
+                    opacity: '0.5',
+                    height: '150px',
+                    width: '150px'
+                });
+            });
+        
+        });
+
+
+
+           
+             /*   $("#block1").hover(function(){
+                    $("#block1").css("transform","translate(200px,0)");
+                    $("#block1").css("opacity", 1);
+            
+                });
+            
+            
+                $("#block2").hover(function(){
+                    $("#block2").css("transform","translate(200px,0)");
+                    $("#block2").css("opacity", 1);
+            
+                
+            });
+          
+                $("#block3").hover(function(){
+                    $("#block3").css("transform","translate(200px,0)");
+                    $("#block3").css("opacity", 1);});*/
+            
+            
+                 
+                   /* $(document).ready(function(){
+                        $("#block2").hover(function(){
+                       
+                            
+                        $("#block2").toggle("slide");
+                        });
+                    });*/
+
+
+/*
+        $('#block1').hover(function(){
+      $("#block1").slideToggle();
+    });*/
+
+
+   
+    
+
+
+
+
+                   /* $(document).ready(function(){
+                        $("#second").click(function(){
+                            $("#myDIV2").slideToggle("slow");
+                        });
+                    });
+                    $(document).ready(function(){
+                        $("#third").click(function(){
+                            $("#myDIV3").slideToggle("slow");
+                        });
+                    });
+ */
